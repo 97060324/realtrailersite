c324a6dbee20256fb2fa2a9a4fbd38ed11a7bd52
97060324 <97060324@st.deltion.nl>
2020-08-28 20:53:25 +0200

    Updatjejejee

--- src/components/Recent.vue
+++ src/components/Recent.vue
@@ -1,9 +1,40 @@
 <template>
-  <h1></h1>
+  <div class="body">
+    <carousel :perPage="1.45" :pagination-enabled="false">
+      <slide>
+        <a href="trialer1"><img src="../assets/Trailers/trailer1.png"></a>
+      </slide>
+      <slide>
+        <a href="trailer2"><img src="../assets/Trailers/trailer2.png"></a>
+      </slide>
+      <slide>
+        <a href="trailer2"><img src="../assets/Trailers/trailer3.png"></a>
+      </slide>
+      <slide>
+        <a href="trailer2"><img src="../assets/Trailers/trailer4.png"></a>
+      </slide>
+      <slide>
+        <a href="trailer2"><img src="../assets/Trailers/trailer5.png"></a>
+      </slide>
+    </carousel>
+
+    <h2 class="trialerofw">Trailer of the week</h2>
+    <div class="totw">
+      <img class="image-totw" src="../assets/Trailers/trailer3.png">
+    </div><br>
+    <div class="totw">
+      <img class="image-totw" src="../assets/Trailers/trailer4.png">
+    </div>
+  </div>
 </template>
 
 <script>
+import { Carousel, Slide } from 'vue-carousel';
+
 export default {
-  
+  components: {
+    Carousel,
+    Slide
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import './css/style.css'
 import './css/html.css'
 
+import VueCarousel from 'vue-carousel';
+Vue.use(VueCarousel);
+
 Vue.config.productionTip = false
 
 new Vue({
