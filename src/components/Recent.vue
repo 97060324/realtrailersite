eaa8cbe79ed748131316020b9bde53b8cb4d558c
David <60575606+97060324@users.noreply.github.com>
2020-08-31 12:23:23 +0200

    Slide 90% responsive

--- src/components/Recent.vue
+++ src/components/Recent.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="body">
-    <carousel :perPage="1.45" :loop="true" :pagination-enabled="false" :autoplay="true">
-      <slide>
+    <div class="recenttrailer">
+      <carousel :perPageCustom="[[768, 5], [1024, 4]]" :loop="true" :pagination-enabled="false" :autoplay="true">
+      <slide class="slide">
         <a href="trialer1"><img src="../assets/Trailers/trailer1.png"></a>
       </slide>
-      <slide>
+      <slide class="slide">
         <a href="trailer2"><img src="../assets/Trailers/trailer2.png"></a>
       </slide>
-      <slide>
+      <slide class="slide">
         <a href="trailer3"><img src="../assets/Trailers/trailer3.png"></a>
       </slide>
-      <slide>
+      <slide class="slide">
         <a href="trailer4"><img src="../assets/Trailers/trailer4.png"></a>
       </slide>
-      <slide>
+      <slide class="slide">
         <a href="trailer5"><img src="../assets/Trailers/trailer5.png"></a>
       </slide>
     </carousel>
+    </div>
   </div>
 </template>
 
