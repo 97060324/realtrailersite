463daf3dea5d9cb627e421b25d7f1afe5b97e537
97060324 <97060324@st.deltion.nl>
2020-09-02 20:53:53 +0200

    Json added only configure

--- src/components/Recent.vue
+++ src/components/Recent.vue
@@ -18,13 +18,35 @@
         <a href="trailer5"><img src="../assets/Trailers/trailer5.png"></a>
       </slide>  
     </carousel>
+
+    <ul>
+      <li v-for="maintrailer in maintrailers" :key="maintrailer.id">
+        {{ maintrailer }}
+      </li>
+    </ul>
     </div>
   </div>
 </template>
 
 <script>
 import { Carousel, Slide } from 'vue-carousel';
+import axios from 'axios';
+
 export default {
+  data() {
+    return {
+      maintrailers: [],
+    }
+  },
+  async created() {
+    try {
+      const red = await axios.get('https://davidvandenpol.nl/json/cardtrailer.json');
+
+      this.maintrailer = red.data;
+    } catch {
+      console.error();
+    }
+  },
   components: {
     Carousel,
     Slide
--- src/components/TrailerOfTheWeek.vue
+++ src/components/TrailerOfTheWeek.vue
@@ -22,6 +22,5 @@
 
 <script>
 export default {
-  
 }
 </script>
