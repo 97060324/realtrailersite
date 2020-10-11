03265ba235d746f91eb3816c0812ba869d449c12
97060324 <97060324@st.deltion.nl>
2020-10-11 20:24:33 +0200

    Carousel werkt alleen gekke bugg

--- src/components/TrailerOverview.vue
+++ src/components/TrailerOverview.vue
@@ -1,19 +1,47 @@
 <template>
     <div class="trailer-overview">
-        <h1>hoi</h1>
-        <h1 @click="closeTrailerOverview()">Go back</h1>
+        <carousel :perPage="1" :paginationEnabled="false" :autoplay="true">
+            <slide v-for="trailer in trailers" :key="trailer.id">
+                <img class="trailer-overview-backgroundimage" :src="trailer.image">
+                <div class="trailer-carousel">
+                    <img class="trailer-carousel-image" :src="trailer.image">
+                </div>  
+            </slide>
+        </carousel>
     </div>
 </template>
 
 
 <script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import { Carousel, Slide } from 'vue-carousel';
+
+Vue.use(VueAxios,axios)
 export default {
+    data() {
+        return {
+            trailers: undefined,
+        }
+    },
+    mounted() {
+        Vue.axios.get('https://api.npoint.io/1f7da34f4106e07d3fc3')
+
+        .then((resp)=>{
+        this.trailers=resp.data.traileroftheweek;
+        })
+    },
     methods: {
         closeTrailerOverview() {
             document.getElementsByClassName('trailer-overview')[0].style.display = 'none';
             document.getElementsByClassName('body')[0].style.display = 'block';
             document.getElementsByClassName('title')[0].style.display = 'block';
         }
+    },
+     components: {
+        Carousel,
+        Slide
     }
 }
 </script>
